53e2c55727c80064dcaa1caf8d2c99e865d7e92c
“Camilo” <camiloelrey2014@gmail.com>
2016-09-15 12:11:21 -0500

    Ejercicio Terminado 100%

---
+++ src/clases/Cuenta.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package clases;
+
+/**
+ *
+ * @author yescobar7
+ */
+public class Cuenta {
+private long numero_cuenta;
+private long numero_id;
+private double saldo_actual;
+
+    public Cuenta(long numero_cuenta, long numero_id, double saldo_actual) {
+        this.numero_cuenta = numero_cuenta;
+        this.numero_id = numero_id;
+        this.saldo_actual = saldo_actual;
+    }
+    public Cuenta(long numero_cuenta, long numero_identificacion){
+    this.numero_cuenta= numero_cuenta;
+    this.numero_id= numero_identificacion;
+    this.saldo_actual= 0;
+    }
+
+    public long getNumero_cuenta() {
+        return numero_cuenta;
+    }
+
+    public void setNumero_cuenta(long numero_cuenta) {
+        this.numero_cuenta = numero_cuenta;
+    }
+
+    public long getNumero_id() {
+        return numero_id;
+    }
+
+    public void setNumero_id(long numero_id) {
+        this.numero_id = numero_id;
+    }
+
+    public double getSaldo_actual() {
+        return saldo_actual;
+    }
+
+    public void setSaldo_actual(double saldo_actual) {
+        this.saldo_actual = saldo_actual;
+    }
+    
+    public void actualizar_saldo(double ian){
+    double aux, aux2;
+    aux= this.getSaldo_actual()*(ian/365);
+    aux2= this.getSaldo_actual()+aux;
+    this.setSaldo_actual(aux);
+    }
+
+    public void ingresar(double Ingreso){
+    double aux;
+    aux= this.getSaldo_actual()+Ingreso;
+    this.setSaldo_actual(aux);
+    }
+    public void retirar(double egreso){
+    double aux;
+    aux= this.getSaldo_actual() - egreso;
+    this.setSaldo_actual(aux);
+    }
+    public String mostrar(){
+    String aux;
+    aux= "No. de la cuenta: "+this.getNumero_cuenta()+"\n"
+        +"No. de identificacion: "+this.getNumero_id()+"\n"
+        +"Saldo ACtual: "+this.getSaldo_actual();
+    return aux;
+    }
+    }
